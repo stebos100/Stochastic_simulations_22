90a974839c7af1eb1ecdead67ee3c56a161a9754
stebos100 <stebos100@gmail.com>
2022-11-15 09:30:21 +0100

    simulation csv
    second simulation for the lhs

--- Plot_mandel.py
+++ Plot_mandel.py
@@ -323,6 +323,103 @@ class mandeL_plot:
             am = np.concatenate((am, area), axis = 0)
         
         return am
+
+    def orthogonal_sampling(self, num_runs, nsamples, numiterations):
+    
+        real_min = self.minx
+        real_max = self.maxx
+        imaginary_min = self.miny
+        imaginary_max = self.maxy
+        
+        # make x an y spaces to form the 2d hypercube just like LHS
+        x = np.linspace(real_min, real_max, nsamples+1)
+        y = np.linspace((imaginary_min), (imaginary_max), nsamples+1)
+
+        # make a list with x ranges consisting of pairs of x values that are consecutive in the linspace. 
+        x_ranges = []
+        y_ranges = []
+
+        for i in range(1, len(x)):
+            x_ranges.append([x[i - 1], x[i]])
+            y_ranges.append([y[i - 1], y[i]])
+        # make the subspaces in the hypercube, this is specific for orthogonal
+        x_subspaces = np.arange(0, nsamples+np.sqrt(nsamples), np.sqrt(nsamples))
+        y_subspaces = np.arange(0, nsamples+np.sqrt(nsamples), np.sqrt(nsamples))
+
+        # Make the indices that will be shuffled each run
+        x_indices = [i for i in range(nsamples)]
+        y_indices = [i for i in range(nsamples)]
+        
+        #Create a list with the intervals between the blocks 
+        block_interval = []
+        for i in range(1, len(x_subspaces)):
+            block_interval.append([int(x_subspaces[i-1]), int(x_subspaces[i])])
+        #print(block_interval)    
+        areas=[]
+
+        for run in range(num_runs):
+            in_mandel = 0
+            total_drawn = nsamples
+            area_T =  np.abs((real_min - real_max))*np.abs(imaginary_max - imaginary_min)
+            samp = []
+
+            for sam in range(nsamples):
+                
+                random.shuffle(x_indices)
+                random.shuffle(y_indices)
+                # loop through the blocks with the previously determined ranges/intervals
+                for i in block_interval:
+                    for j in block_interval:
+                        # create list with individual blocks in the interval range
+                        x_blocks = [x for x in range(j[0], j[1])]
+                        y_blocks = [y for y in range(i[0], i[1])]
+
+                        
+                        coordinate_x = 0
+                        coordinate_y = 0
+
+                        # loop through available indices
+                        for k in x_indices:
+                        # if an available index is in slice of indices
+                        #print(k)
+                            if k in x_blocks:
+                                # set x coordinate to available index
+                                coordinate_x = k
+                                # remove available index
+                                x_indices.remove(k)
+                                # break out of the for-loop
+                                break
+
+                        # similar for y
+                        for k in y_indices:
+                            if k in y_blocks:
+                                coordinate_y = k
+                                y_indices.remove(k)
+                                break
+
+                                # break out of for-loop and move to next block
+
+                        # from x-range, take strata with same x coordinate
+                        #print(coordinate_x)
+                        #print("")
+                        #print(x_ranges)
+                        #print("")
+                        x_sample_range = x_ranges[coordinate_x]
+                        y_sample_range = y_ranges[-(coordinate_y+1)]
+                        #print(x_sample_range)
+
+                        sample = complex(random.uniform(x_sample_range[0], x_sample_range[1]), random.uniform(y_sample_range[0], y_sample_range[1])) 
+
+                        if self.within_mandel(numiterations, sample):
+                            in_mandel += 1
+
+            ratio_inmandel = (in_mandel/total_drawn)
+            area_mandel = ratio_inmandel*area_T        
+
+            areas.append(area_mandel)
+
+        return areas         
+        
         
 
 
@@ -402,7 +499,7 @@ plt.savefig("mandel_plot_second.pdf", dpi = 900,  bbox_inches = 'tight', format 
 
 
 #%%
-
+#%#%#%# start here #%#%#%#%#%#%#%#
 #%#%%#%#%#%#%#%#%#%#%#%#%#%#% Creating a second class instance for testing convergence #%#%#%#%#%#%#%#%#%#%#%#
 RE_START = -2.0
 RE_END = 1.0
@@ -413,6 +510,8 @@ its = 1000
 
 mandel= mandeL_plot(RE_START, RE_END, IM_START, IM_END, image, its)
 
+#%%
+mandel.orthogonal_sampling(10, 10, 10)
 #%%
 #%#%#%#%#%#%#%#%#%#%#%#%# CHECKING THE NUMBER OF SAMPLES REQUIRED #%#%#%#%#%#%#%#%#%#%#%#
 
@@ -496,6 +595,7 @@ areas_matrix = np.zeros(shape = (1, num_runs), dtype = np.float32)
 from numpy import genfromtxt
 my_data = genfromtxt('AM_one.csv', delimiter=',')
 my_data.shape
+
 #%%
 """firstly we want to evaluate the normality of the results to see if this is satisfactory 
 from here we can then include confidence intervals and use normal distrbution 
@@ -573,9 +673,14 @@ areas_matrix = np.zeros(shape = (1, num_runs), dtype = np.float32)
 my_data2 = genfromtxt('AM_two.csv', delimiter=',')
 #%#%#%#%#%#%#%#%#%#%#%#%#%#% generating the mean of all simulations along the rows #%#%#%#%#%#%
 #%%
+my_data2.shape
+
+
+#%%
 #%#%#%#%#%#%#%#%#%#%#%#%#%#%#% plotting the mean to the true solution to test the convergence of the solution #%#%#%#%#%#%#%#%#%#%#%#%#%#%#
 mean2 = np.mean(my_data2, axis = 1)
 mean2 = mean2[1:]
+#%%
 cis2 = calculate_confidence_interval(my_data2[1:])
 cis2 = cis2[1:]
 #%%
@@ -689,7 +794,41 @@ num_runs = 1000
 num_samples = 2000
 num_iterations = np.arange(100, 12000, 100, dtype = np.int16)
 areas_matrix = np.zeros(shape = (1, num_runs), dtype = np.float32)
+#%%
+#%#%#%#%#%#%# this has already been performed #%#%#%#%#%#%#%#%#%%#%#%#%#%#%#
+# areas_lhs_its =mandel.return_area_matrix_constant_iterations_LHS(num_runs, num_samples, num_iterations, areas_matrix)
+# np.savetxt("AM_LHS_ITS.csv", areas_lhs_its, delimiter=",")
+#%%
+areas_lhs_its = genfromtxt('AM_LHS_ITS.csv', delimiter=',')
+areas_lhs_its = areas_lhs_its[1:]
+areas_lhs_its
+# %%
+#%%
+mean_lhs_its = np.mean(areas_lhs_its, axis = 1)
+mean_lhs_its.shape
 
 #%%
-areas_lhs_its =mandel.return_area_matrix_constant_iterations_LHS(num_runs, num_samples, num_iterations, areas_matrix)
-#np.savetxt("AM_LHS.csv", areas_lhs, delimiter=",")
+ci_its = calculate_confidence_interval(areas_lhs_its)
+ci_its = ci_its[2:]
+ci_its.shape
+#%%
+plt.style.use('seaborn')
+fig, ax = plt.subplots(figsize = (8,8))
+ax.plot(num_iterations, mean_lhs_its[1:], label = 'Average Area for for 1000 runs, 2000 samples')
+ax.axhline(y = 1.5064, color = 'r', linestyle = '--', label = 'True value of the area of the MandelBrot set')
+plt.fill_between(num_iterations, ci_its[:, :1].reshape(119), ci_its[:, 1:].reshape(119),  color = 'blue', alpha = 0.15, label = '95% confidence interval')
+plt.ylabel('Area of MandelBrot set', fontsize = 14)
+plt.xlabel('Number of iterations', fontsize = 14)
+plt.title('Convergent behaviour for increasing number of iterations', fontsize = 16)
+plt.legend(fontsize = 12)
+#%%
+std_its = np.std(areas_lhs_its, axis = 1)
+plt.style.use('seaborn')
+fig, ax = plt.subplots(figsize = (8,8))
+ax.plot( num_iterations, (std_its[1:]), label = 'Standard deviation')
+plt.xlabel('Number of iterations', fontsize = 14)
+plt.ylabel('Standard deviation of sample Area', fontsize = 14)
+plt.title('Convergent behaviour of sample Standard deviation', fontsize = 16)
+
+# %%
+
