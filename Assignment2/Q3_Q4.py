80f34dd937009948f297de5ed700f92e061dc231
stebos100 <stebos100@gmail.com>
2022-12-04 14:42:39 +0100

    update
    update

--- Assignment2/Q3_Q4.py
+++ Assignment2/Q3_Q4.py
@@ -176,34 +176,135 @@ theory = []
 for rho, mu in zip(rho_range, mu_range):
     theory.append(Functions.SPTF(rho, mu, 1))
 #%%
-fig, ax1 = plt.subplots(figsize = (8,8))
-fig2, ax2 = plt.subplots(figsize = (8,8))
+fig, ax1 = plt.subplots(figsize = (7,7))
+fig2, ax2 = plt.subplots(figsize = (7,7))
+
+ax1.plot(rho_range , theory, linestyle = '--', linewidth = 1, label = "Theoretical result for {} server".format(1))
+
 for i in n_servers:
     if i == 1:
-        ax1.scatter(p_range, relavant_means_MM[0:i*20], marker = "^", label = 'simulation values for {} server(s)'.format(i))
+        ax1.scatter(p_range, relavant_means_MM[0:i*20], marker = "^", label = 'simulation values for {} server'.format(i))
         ax1.fill_between(p_range, relavant_means_MM[0:i*20] + 0.1, relavant_means_MM[0:i*20] - 0.1, alpha = 0.3, label = "0.05 width tolerance")
-        ax2.plot(p_range, relavant_std_MM[0:i*20], marker = "^", label = 'standard deviation for {} server(s)'.format(i))
+        ax2.plot(p_range, relavant_std_MM[0:i*20], marker = "^", label = 'standard deviation for {} server(s)'.format(i), linewidth = 2, markersize = 8)
     elif i == 2:
         ax1.plot(p_range, relavant_means_MM[((i)*10):((i+2)*10)], label = 'simulation values for {} server(s)'.format(i))
-        ax2.plot(p_range, relavant_std_MM[((i)*10):((i+2)*10)], marker = "o", linestyle = "--", label = 'standard deviation for {} server(s)'.format(i))
+        ax2.plot(p_range, relavant_std_MM[((i)*10):((i+2)*10)], marker = "o", linestyle = "--", label = 'standard deviation for {} server(s)'.format(i), linewidth = 2, markersize = 8)
 
     elif i == 4:
         ax1.plot(p_range, relavant_means_MM[((i)*10):((i+2)*10)], label = 'simulation values for {} server(s)'.format(i))
-        ax2.plot(p_range, relavant_std_MM[((i)*10):((i+2)*10)], marker = "8", label = 'standard deviation for {} server(s)'.format(i))
+        ax2.plot(p_range, relavant_std_MM[((i)*10):((i+2)*10)], marker = "8", linestyle  = "-.", label = 'standard deviation for {} server(s)'.format(i), linewidth = 2, markersize = 8)
 
 
-ax1.plot(rho_range , theory, linestyle = '--', linewidth = 1, label = "Theoretical result for {} server".format(i))
 ax1.legend(fontsize = 13)
-ax1.set_xlabel(r'$\rho$', fontsize = 17)
-ax1.set_ylabel("Waiting time", fontsize = 14)
-ax1.set_title("Waiting times for M/M/1 - M/M/n vs amount of servers available", fontsize  = 15)
+ax1.set_xlabel( "Utilization rate " r'$\rho$', fontsize = 15)
+ax1.set_ylabel(r'$\bar{W}$', fontsize = 15)
+ax1.set_title("Waiting times for SJFS/1 - SJFS/n Queuing simulation", fontsize  = 15)
+ax1.tick_params(axis='both', which='major', labelsize=13)
+
 
 ax2.legend(fontsize = 13)
-ax2.set_xlabel(r'$\rho$', fontsize = 17)
-ax2.set_ylabel("Standard deviation", fontsize = 14)
-ax2.set_title("Standard deviation for M/M/1 - M/M/n Queue simulation", fontsize  = 15)
+ax2.set_xlabel("Utilization rate " r'$\rho$', fontsize = 15)
+ax2.set_ylabel(r'$S[\bar{W}]$', fontsize = 15)
+ax2.set_title("Standard deviation for SJFS/1 - SJFS/n Queuing simulation", fontsize  = 15)
+ax2.tick_params(axis='both', which='major', labelsize=12)
 #%%
 #%% 
+#%#%#%#%#%#%#%#%#%%#%#%##%#%#%#%#%#%#%#%#%%#%#%##%#%#%#%#%#%#%#%#%%#%#%##%#%#%#%#%#%#%#%#%%#%#%##%#%#%#%#%#%#%#%#%%
+#%#%#%#%#%#%#%#%#%%#%#%# investigating deterministic distributions #%#%#%#%#%#%#%#%#%%#%#%##%#%#%#%#%#%#%#%#%%#%#%#
+#global variables
+# %%
+n_samples = 200000
+n_servers = np.array([1,2,4])
+steps = 20
+arrival_rate = n_servers
+p_min = 0.5
+p_max = 0.95
+p_range = np.linspace(p_min, p_max, steps)
+service_rate = (1 / p_range)
+runs = 25
+
+waiting_times_MDN_stacked = np.zeros((1, runs))
+
+#%%
+# for i in range(len(n_servers)):
+#     for j in tqdm(range(steps), desc=f'calculate waiting times for n_server {n_servers[i]} and step {j}'):
+#         waiting_times_MDN_stacked_temp = np.zeros((1, n_samples))
+#         for k in range(runs):
+#             env = simpy.Environment()
+#             servers3 = simpy.PriorityResource(env, capacity=n_servers[i])
+#             waiting_times = []
+#             setup3 = DES_MD_LT(env, arrival_rate[i], service_rate[j], servers3, waiting_times, n_samples,'deterministic')
+#             env.run(until=setup3.num_samples_count)
+#             waiting_times_MDN_stacked_temp = np.vstack((waiting_times_MDN_stacked_temp, setup3.waiting_times[:n_samples]))   
+#         appending = np.mean(waiting_times_MDN_stacked_temp[1:], axis = 1)
+#         apend = appending.reshape(1, appending.shape[0])
+#         waiting_times_MDN_stacked = np.vstack((waiting_times_MDN_stacked,apend))
+
+# waiting_times_MDN_stacked = waiting_times_MDN_stacked[1:]
+# np.savetxt("MDN_0.5_0.95.csv", waiting_times_MDN_stacked, delimiter=",")
+
+#%%
+waiting_times_MDN_stacked = genfromtxt("MDN_0.5_0.95.csv", delimiter=',')
+
+relavant_std_MD = np.std(waiting_times_MDN_stacked, axis = 1)
+relavant_means_MD = np.mean(waiting_times_MDN_stacked, axis = 1)
+
+#%%
+rho_range = np.linspace(p_min, p_max, 1000)
+mu_range = 1 / rho_range
+theoretical_det = np.zeros((1,len(rho_range)))
+for i in n_servers:
+    values = []
+    for rho, mu in zip(rho_range, mu_range):
+        values.append(Functions.MDn(rho, mu, i))
+
+    theoretical_det = np.vstack((theoretical_det, values))
+    
+theoretical_det = theoretical_det[1:]
+#%%
+
+fig5, ax5 = plt.subplots(figsize = (8,8))
+
+l1 = ax5.scatter(p_range, relavant_means_MD[0:20], marker = "^", s = 80, c ='teal')
+m1 = ax5.plot(rho_range , theoretical_det[0, :], linestyle = '--', linewidth = 1.5, c = 'teal')
+
+l2 = ax5.scatter(p_range, relavant_means_MD[20:40], s = 80, c = 'm')
+m2 = ax5.plot(rho_range , theoretical_det[1, :], linestyle = '--', linewidth = 1.5, c = 'm')
+
+l3 = ax5.scatter(p_range, relavant_means_MD[40:60], s = 80, c  = 'chocolate')
+m3 = ax5.plot(rho_range , theoretical_det[2, :], linestyle = '--', linewidth = 1.5, c = 'chocolate')
+
+p1 = ax5.fill_between(p_range, relavant_means_MD[0:20] + 0.1, relavant_means_MD[0:20] - 0.1, alpha = 0.4, color = "mediumpurple")
+p2 = ax5.fill_between(p_range, relavant_means_MD[20:40] + 0.1, relavant_means_MD[20:40] - 0.1, alpha = 0.4, color = "mediumpurple")
+p3 = ax5.fill_between(p_range, relavant_means_MD[40:60] + 0.1, relavant_means_MD[40:60]- 0.1, alpha = 0.4, color = "mediumpurple")
+
+
+ax5.legend(fontsize = 13)
+ax5.set_xlabel( "Utilization rate [" r'$\rho$'"]", fontsize = 15)
+ax5.set_ylabel(r'$\bar{W}$', fontsize = 16)
+ax5.set_title("Waiting times for M/D/1 - M/D/n Queueing simulation", fontsize  = 15)
+ax5.tick_params(axis='both', which='major', labelsize=13)
+ax5.legend(["simulation values for 1 server", "Theoretical result for 1 server", \
+   "simulation values for 2 server(s)" , "Theoretical result for 2 server(s)", "simulation values for 4 server(s)", \
+       "Theoretical result for 4 server(s)", "0.05 std deviation tolerance value"], fontsize =13)
+fig5.savefig('M_D_N_QUEUE.png', bbox_inches='tight', dpi = 600 )
+
+
+fig6, ax6 = plt.subplots(figsize = (8,8))
+
+ax6.plot(p_range, relavant_std_MD[0:20], marker = "^", label = 'standard deviation for 1 server(s)', linewidth = 2, markersize = 8, c = 'teal')
+ax6.plot(p_range, relavant_std_MD[20:40], marker = "o", linestyle = "--", label = 'standard deviation for 2 server(s)', linewidth = 2, markersize = 8, c = 'm')
+ax6.plot(p_range, relavant_std_MD[40:60], marker = "8", label = 'standard deviation for 4 server(s)', linewidth = 2, markersize = 8, c = 'chocolate')
+
+
+ax6.legend(fontsize = 13)
+ax6.set_xlabel("Utilization rate [" r'$\rho$'"]", fontsize = 15)
+ax6.set_ylabel(r'$S[\bar{W}]$', fontsize = 15)
+ax6.set_title("Standard deviation for M/D/1 - M/D/n Queue simulation", fontsize  = 15)
+ax6.tick_params(axis='both', which='major', labelsize=13)
+fig6.savefig('M_D_N_QUEUE_STD.png', bbox_inches='tight', dpi = 600 )
+
+#%%
 #%#%#%#%#%#%#%#%#%%#%#%##%#%#%#%#%#%#%#%#%%#%#%##%#%#%#%#%#%#%#%#%%#%#%##%#%#%#%#%#%#%#%#%%#%#%#
 #%#%#%#%#%#%#%#%#%#%#%#%#% going to investigate long tailed distribution #%#%#%#%#%#%#%#%#%%#%%#
 
@@ -259,120 +360,46 @@ for i in n_servers:
 theoretical_longtail = theoretical_longtail[1:]
 
 #%%
-fig, ax3 = plt.subplots(figsize = (8,8))
-fig2, ax4 = plt.subplots(figsize = (8,8))
-for i in n_servers:
-    if i == 1:
-        ax3.scatter(p_range, relavant_means_ML[0:i*20], marker = "^", label = 'simulation values for {} server(s)'.format(i))
-        ax3.fill_between(p_range, relavant_means_ML[0:i*20] + 0.2, relavant_means_ML[0:i*20] - 0.2, alpha = 0.3, label = "0.05 width tolerance")
-        ax3.plot(rho_range , theoretical_longtail[0, :], linestyle = '--', linewidth = 1, label = "Theoretical result for {} server".format(i))
-        ax4.plot(p_range, relavant_std_MM[0:i*20], marker = "^", label = 'standard deviation for {} server(s)'.format(i))
-    elif i == 2:
-        ax3.scatter(p_range, relavant_means_ML[((i)*10):((i+2)*10)], label = 'simulation values for {} server(s)'.format(i))
-        ax3.fill_between(p_range, relavant_means_ML[((i)*10):((i+2)*10)] + 0.2, relavant_means_ML[((i)*10):((i+2)*10)] - 0.2, alpha = 0.3, label = "0.05 width tolerance")
-        ax3.plot(rho_range , theoretical_longtail[1, :], linestyle = '--', linewidth = 1, label = "Theoretical result for {} server".format(i))
-        ax4.plot(p_range, relavant_std_MM[((i)*10):((i+2)*10)], marker = "o", linestyle = "--", label = 'standard deviation for {} server(s)'.format(i))
 
-    elif i == 4:
-        ax3.scatter(p_range, relavant_means_ML[((i)*10):((i+2)*10)], label = 'simulation values for {} server(s)'.format(i))
-        ax3.fill_between(p_range, relavant_means_ML[((i)*10):((i+2)*10)] + 0.2, relavant_means_ML[((i)*10):((i+2)*10)]- 0.2, alpha = 0.3, label = "0.05 width tolerance")
-        ax3.plot(rho_range , theoretical_longtail[0, :], linestyle = '--', linewidth = 1, label = "Theoretical result for {} server".format(i))
-        ax4.plot(p_range, relavant_std_MM[((i)*10):((i+2)*10)], marker = "8", label = 'standard deviation for {} server(s)'.format(i))
+fig3, ax3 = plt.subplots(figsize = (8,8))
+
+l1 = ax3.scatter(p_range, relavant_means_ML[0:20], marker = "^", s = 80)
+m1 = ax3.plot(rho_range , theoretical_longtail[0, :], linestyle = '--', linewidth = 1.5)
+
+l2 = ax3.scatter(p_range, relavant_means_ML[20:40], s = 80)
+m2 = ax3.plot(rho_range , theoretical_longtail[1, :], linestyle = '--', linewidth = 1.5)
+
+l3 = ax3.scatter(p_range, relavant_means_ML[40:60], s = 80)
+m3 = ax3.plot(rho_range , theoretical_longtail[2, :], linestyle = '--', linewidth = 1.5)
+
+p1 = ax3.fill_between(p_range, relavant_means_ML[0:20] + 0.2, relavant_means_ML[0:20] - 0.2, alpha = 0.4, color = "mediumpurple")
+p2 = ax3.fill_between(p_range, relavant_means_ML[20:40] + 0.2, relavant_means_ML[20:40] - 0.2, alpha = 0.4, color = "mediumpurple")
+p3 = ax3.fill_between(p_range, relavant_means_ML[40:60] + 0.2, relavant_means_ML[40:60]- 0.2, alpha = 0.4, color = "mediumpurple")
 
 
 ax3.legend(fontsize = 13)
-ax3.set_xlabel(r'$\rho$', fontsize = 17)
-ax3.set_ylabel("Waiting time", fontsize = 14)
-ax3.set_title("Waiting times for M/L/1 - M/L/n Queueing simulation", fontsize  = 15)
+ax3.set_xlabel( "Utilization rate [" r'$\rho$'"]", fontsize = 15)
+ax3.set_ylabel(r'$\bar{W}$', fontsize = 15)
+ax3.set_title("Waiting times for M/L/1 - M/L/n Queueing simulation", fontsize  = 16)
+ax3.tick_params(axis='both', which='major', labelsize=13)
+ax3.legend(["simulation values for 1 server", "Theoretical result for 1 server", \
+   "simulation values for 2 server(s)" , "Theoretical result for 2 server(s)", "simulation values for 4 server(s)", \
+       "Theoretical result for 4 server(s)", "0.05 std deviation tolerance value"], fontsize =13)
+
+fig3.savefig('M_L_N_QUEUE.png', bbox_inches='tight', dpi = 600)
+
+fig4, ax4 = plt.subplots(figsize = (8,8))
+
+ax4.plot(p_range, relavant_std_ML[0:20], marker = "^", label = 'standard deviation for 1 server(s)', linewidth = 2, markersize = 8)
+ax4.plot(p_range, relavant_std_ML[20:40], marker = "o", linestyle = "--", label = 'standard deviation for 2 server(s)', linewidth = 2, markersize = 8)
+ax4.plot(p_range, relavant_std_ML[40:60], marker = "8", label = 'standard deviation for 4 server(s)', linewidth = 2, markersize = 8)
+
 
 ax4.legend(fontsize = 13)
-ax4.set_xlabel(r'$\rho$', fontsize = 17)
-ax4.set_ylabel("Standard deviation", fontsize = 14)
-ax4.set_title("Standard deviation for M/L/1 - M/L/n Queue simulation", fontsize  = 15)
+ax4.set_xlabel("Utilization rate [" r'$\rho$'"]", fontsize = 15)
+ax4.set_ylabel(r'$S[\bar{W}]$', fontsize = 15)
+ax4.set_title("Standard deviation for M/L/1 - M/L/n Queue simulation", fontsize  = 16)
+ax4.tick_params(axis='both', which='major', labelsize=13)
+fig4.savefig('M_L_N_QUEUE_STD.png', bbox_inches='tight', dpi = 600 )
 
-# %%
-#%#%#%#%#%#%#%#%#%%#%#%##%#%#%#%#%#%#%#%#%%#%#%##%#%#%#%#%#%#%#%#%%#%#%##%#%#%#%#%#%#%#%#%%#%#%##%#%#%#%#%#%#%#%#%%
-#%#%#%#%#%#%#%#%#%%#%#%# investigating deterministic distributions #%#%#%#%#%#%#%#%#%%#%#%##%#%#%#%#%#%#%#%#%%#%#%#
-#global variables
-# %%
-n_samples = 200000
-n_servers = np.array([1,2,4])
-steps = 20
-arrival_rate = n_servers
-p_min = 0.5
-p_max = 0.95
-p_range = np.linspace(p_min, p_max, steps)
-service_rate = (1 / p_range)
-runs = 25
-
-waiting_times_MDN_stacked = np.zeros((1, runs))
-
-#%%
-# for i in range(len(n_servers)):
-#     for j in tqdm(range(steps), desc=f'calculate waiting times for n_server {n_servers[i]} and step {j}'):
-#         waiting_times_MDN_stacked_temp = np.zeros((1, n_samples))
-#         for k in range(runs):
-#             env = simpy.Environment()
-#             servers3 = simpy.PriorityResource(env, capacity=n_servers[i])
-#             waiting_times = []
-#             setup3 = DES_MD_LT(env, arrival_rate[i], service_rate[j], servers3, waiting_times, n_samples,'deterministic')
-#             env.run(until=setup3.num_samples_count)
-#             waiting_times_MDN_stacked_temp = np.vstack((waiting_times_MDN_stacked_temp, setup3.waiting_times[:n_samples]))   
-#         appending = np.mean(waiting_times_MDN_stacked_temp[1:], axis = 1)
-#         apend = appending.reshape(1, appending.shape[0])
-#         waiting_times_MDN_stacked = np.vstack((waiting_times_MDN_stacked,apend))
-
-# waiting_times_MDN_stacked = waiting_times_MDN_stacked[1:]
-# np.savetxt("MDN_0.5_0.95.csv", waiting_times_MDN_stacked, delimiter=",")
-
-#%%
-waiting_times_MDN_stacked = genfromtxt("MDN_0.5_0.95.csv", delimiter=',')
-
-relavant_std_MD = np.std(waiting_times_MDN_stacked, axis = 1)
-relavant_means_MD = np.mean(waiting_times_MDN_stacked, axis = 1)
-
-#%%
-rho_range = np.linspace(p_min, p_max, 1000)
-mu_range = 1 / rho_range
-theoretical_det = np.zeros((1,len(rho_range)))
-for i in n_servers:
-    values = []
-    for rho, mu in zip(rho_range, mu_range):
-        values.append(Functions.MDn(rho, mu, i))
-
-    theoretical_det = np.vstack((theoretical_det, values))
-    
-theoretical_det = theoretical_det[1:]
-#%%
-fig, ax5 = plt.subplots(figsize = (8,8))
-fig2, ax6 = plt.subplots(figsize = (8,8))
-for i in n_servers:
-    if i == 1:
-        ax5.scatter(p_range, relavant_means_MD[0:i*20], marker = "^", label = 'simulation values for {} server(s)'.format(i))
-        ax5.fill_between(p_range, relavant_means_MD[0:i*20] + 0.1, relavant_means_MD[0:i*20] - 0.1, alpha = 0.3, label = "0.05 width tolerance")
-        ax5.plot(rho_range , theoretical_det[0, :], linestyle = '--', linewidth = 1, label = "Theoretical result for {} server".format(i))
-        ax6.plot(p_range, relavant_std_MM[0:i*20], marker = "^", label = 'standard deviation for {} server(s)'.format(i))
-    elif i == 2:
-        ax5.scatter(p_range, relavant_means_MD[((i)*10):((i+2)*10)], label = 'simulation values for {} server(s)'.format(i))
-        ax5.fill_between(p_range, relavant_means_MD[((i)*10):((i+2)*10)] + 0.1, relavant_means_MD[((i)*10):((i+2)*10)] - 0.1, alpha = 0.3, label = "0.05 width tolerance")
-        ax5.plot(rho_range , theoretical_det[1, :], linestyle = '--', linewidth = 1, label = "Theoretical result for {} server".format(i))
-        ax6.plot(p_range, relavant_std_MM[((i)*10):((i+2)*10)], marker = "o", linestyle = "--", label = 'standard deviation for {} server(s)'.format(i))
-
-    elif i == 4:
-        ax5.scatter(p_range, relavant_means_MD[((i)*10):((i+2)*10)], label = 'simulation values for {} server(s)'.format(i))
-        ax5.fill_between(p_range, relavant_means_MD[((i)*10):((i+2)*10)] + 0.1, relavant_means_MD[((i)*10):((i+2)*10)]- 0.1, alpha = 0.3, label = "0.05 width tolerance")
-        ax5.plot(rho_range , theoretical_det[0, :], linestyle = '--', linewidth = 1, label = "Theoretical result for {} server".format(i))
-        ax6.plot(p_range, relavant_std_MM[((i)*10):((i+2)*10)], marker = "8", label = 'standard deviation for {} server(s)'.format(i))
-
-
-ax5.legend(fontsize = 13)
-ax5.set_xlabel(r'$\rho$', fontsize = 17)
-ax5.set_ylabel("Waiting time", fontsize = 14)
-ax5.set_title("Waiting times for M/D/1 - M/D/n Queueing simulation", fontsize  = 15)
-
-ax6.legend(fontsize = 13)
-ax6.set_xlabel(r'$\rho$', fontsize = 17)
-ax6.set_ylabel("Standard deviation", fontsize = 14)
-ax6.set_title("Standard deviation for M/D/1 - M/D/n Queue simulation", fontsize  = 15)
-#%%
 # %%
